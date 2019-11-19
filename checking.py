75f9e47d6b9f05f89b22e22a0173673a07d01f34
Lewis-11 <100383365@alumnos.uc3m.es>
2019-11-19 21:27:54 +0100

    ajskdlfñ

--- checking.py
+++ checking.py
@@ -4,14 +4,17 @@ origFile=open("out.txt", "r")
 ourLines=nuestroFile.readlines()
 theirLines=origFile.readlines()
 
-for i in range(len(ourLines)):
-    ourdata=ourLines[i].split(" ")
-    theirdata=theirLines[i].split(" ")
-    for j in range(5):
-        diff= float(ourdata[j])-float(theirdata[j])
-        if diff<0:
-            diff = diff*(-1)
-        if diff>5.0:
-            print("problem found: " + ourdata[j] + " when orig was: " + theirdata[j] + " in line: " + str(i+1))
+if not len(ourLines) == len(theirLines):
+    print("out.txt and out_seq_nuestro.txt dont have the same # of lines")
+else:
+    for i in range(len(ourLines)):
+        ourdata=ourLines[i].split(" ")
+        theirdata=theirLines[i].split(" ")
+        for j in range(5):
+            diff= float(ourdata[j])-float(theirdata[j])
+            if diff<0:
+                diff = diff*(-1)
+            if diff>5.0:
+                print("problem found: " + ourdata[j] + " when orig was: " + theirdata[j] + " in line: " + str(i+1))
 
 
