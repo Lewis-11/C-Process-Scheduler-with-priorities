59d7ca13166bd2e0371bbb5b4fccbd054f381f0f
ricolinn <ricky99013@gmail.com>
2019-11-19 17:32:16 +0100

    Resultados pruebas secuencial

--- script.sh
+++ script.sh
@@ -1,50 +1,65 @@
 #!/bin/bash
-
+#The proportion between asteroids and planets is 70/30
+#Iterations are 50, 100, 200
+#Sum of asteroids and planets is 250, 500 and 1000
 g++ -std=c++14 -Wall -Wextra -Wno-deprecated -Werror -pedantic -pedantic-errors -O3 main.cpp -o main
 
 
-echo "---"
+echo "---ITERATIONS: 50 ---"
 for i in {0..14}
 do
-    ./main    250 50 250 6
+    ./main 175 50 75 6
 done
 echo "---"
+
 for i in {0..14}
 do
-    ./main    250 100 250 6
+    ./main 350 50 150 6
 done
 echo "---"
+
 for i in {0..14}
 do
-    ./main    250 200 250 6
+    ./main 700 50 30 6
 done
 echo "---"
+
+echo "---ITERATIONS: 100 ---"
 for i in {0..14}
 do
-    ./main    500 50 500 6
+    ./main 175 100 75 6
 done
 echo "---"
+
 for i in {0..14}
 do
-    ./main    500 100 500 6
+    ./main 350 100 150 6
 done
 echo "---"
+
 for i in {0..14}
 do
-    ./main    500 200 500 6
+    ./main 700 100 30 6
 done
 echo "---"
+
+echo "---ITERATIONS: 200---"
 for i in {0..14}
 do
-    ./main    1000 50 1000 6
+    ./main 175 200 75 6
 done
 echo "---"
+
 for i in {0..14}
 do
-    ./main    1000 100 1000 6
+    ./main 350 200 150 6
 done
 echo "---"
+
 for i in {0..14}
 do
-    ./main    1000 200 1000 6
+    ./main 700 200 30 6
 done
+echo "---"
+
+echo "Dale Carla, ahora mostráme aunque sea una mijita de pezón"
