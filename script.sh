3ad93a8a46aae5cc1a26a702ec8c2253b257fea3
Lewis-11 <100383365@alumnos.uc3m.es>
2019-11-23 20:06:14 +0100

    sprite

--- script.sh
+++ script.sh
@@ -8,57 +8,57 @@
 echo "---ITERATIONS: 50 ---"
 for i in {0..14}
 do
-    ./main 175 50 75 6
+    ./main_seq 175 50 75 6
 done
 echo "---"
 
 for i in {0..14}
 do
-    ./main 350 50 150 6
+    ./main_seq 350 50 150 6
 done
 echo "---"
 
 for i in {0..14}
 do
-    ./main 700 50 300 6
+    ./main_seq 700 50 300 6
 done
 echo "---"
 
 echo "---ITERATIONS: 100 ---"
 for i in {0..14}
 do
-    ./main 175 100 75 6
+    ./main_seq 175 100 75 6
 done
 echo "---"
 
 for i in {0..14}
 do
-    ./main 350 100 150 6
+    ./main_seq 350 100 150 6
 done
 echo "---"
 
 for i in {0..14}
 do
-    ./main 700 100 300 6
+    ./main_seq 700 100 300 6
 done
 echo "---"
 
 echo "---ITERATIONS: 200---"
 for i in {0..14}
 do
-    ./main 175 200 75 6
+    ./main_seq 175 200 75 6
 done
 echo "---"
 
 for i in {0..14}
 do
-    ./main 350 200 150 6
+    ./main_seq 350 200 150 6
 done
 echo "---"
 
 for i in {0..14}
 do
-    ./main 700 200 300 6
+    ./main_seq 700 200 300 6
 done
 echo "---"
 
